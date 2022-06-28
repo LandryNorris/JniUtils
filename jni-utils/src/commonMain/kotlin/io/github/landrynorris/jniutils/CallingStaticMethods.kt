f57ca7fa5eea219c706cbfc7663184992ca20d62
Landry Norris <landry.norris@live.com>
2022-06-28 18:40:18 -0500

    Fixed names of callStatic*Methods

--- jni-utils/src/commonMain/kotlin/io/github/landrynorris/jniutils/CallingStaticMethods.kt
+++ jni-utils/src/commonMain/kotlin/io/github/landrynorris/jniutils/CallingStaticMethods.kt
@@ -13,74 +13,74 @@ fun CPointer<JNIEnvVar>.getStaticMethodId(clazz: jclass, name: String, signature
     }
 }
 
-fun CPointer<JNIEnvVar>.getStaticObjectMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): jobject? {
+fun CPointer<JNIEnvVar>.callStaticObjectMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): jobject? {
     val method = pointed.pointed?.CallStaticObjectMethodA ?: error("JNI is not Oracle standard")
     return memScoped {
         val ptr = args.toCArray(this)
-        method.invoke(this@getStaticObjectMethod, clazz, methodId, ptr)
+        method.invoke(this@callStaticObjectMethod, clazz, methodId, ptr)
     }
 }
 
-fun CPointer<JNIEnvVar>.getStaticIntMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Int {
+fun CPointer<JNIEnvVar>.callStaticIntMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Int {
     val method = pointed.pointed?.CallStaticIntMethodA ?: error("JNI is not Oracle standard")
     return memScoped {
         val ptr = args.toCArray(this)
-        method.invoke(this@getStaticIntMethod, clazz, methodId, ptr)
+        method.invoke(this@callStaticIntMethod, clazz, methodId, ptr)
     }
 }
 
-fun CPointer<JNIEnvVar>.getStaticLongMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Long {
+fun CPointer<JNIEnvVar>.callStaticLongMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Long {
     val method = pointed.pointed?.CallStaticLongMethodA ?: error("JNI is not Oracle standard")
     return memScoped {
         val ptr = args.toCArray(this)
-        method.invoke(this@getStaticLongMethod, clazz, methodId, ptr)
+        method.invoke(this@callStaticLongMethod, clazz, methodId, ptr)
     }
 }
 
-fun CPointer<JNIEnvVar>.getStaticDoubleMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Double {
+fun CPointer<JNIEnvVar>.callStaticDoubleMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Double {
     val method = pointed.pointed?.CallStaticDoubleMethodA ?: error("JNI is not Oracle standard")
     return memScoped {
         val ptr = args.toCArray(this)
-        method.invoke(this@getStaticDoubleMethod, clazz, methodId, ptr)
+        method.invoke(this@callStaticDoubleMethod, clazz, methodId, ptr)
     }
 }
 
-fun CPointer<JNIEnvVar>.getStaticFloatMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Float {
+fun CPointer<JNIEnvVar>.callStaticFloatMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Float {
     val method = pointed.pointed?.CallStaticFloatMethodA ?: error("JNI is not Oracle standard")
     return memScoped {
         val ptr = args.toCArray(this)
-        method.invoke(this@getStaticFloatMethod, clazz, methodId, ptr)
+        method.invoke(this@callStaticFloatMethod, clazz, methodId, ptr)
     }
 }
 
-fun CPointer<JNIEnvVar>.getStaticShortMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Short {
+fun CPointer<JNIEnvVar>.callStaticShortMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Short {
     val method = pointed.pointed?.CallStaticShortMethodA ?: error("JNI is not Oracle standard")
     return memScoped {
         val ptr = args.toCArray(this)
-        method.invoke(this@getStaticShortMethod, clazz, methodId, ptr)
+        method.invoke(this@callStaticShortMethod, clazz, methodId, ptr)
     }
 }
 
-fun CPointer<JNIEnvVar>.getStaticCharMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Char {
+fun CPointer<JNIEnvVar>.callStaticCharMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Char {
     val method = pointed.pointed?.CallStaticCharMethodA ?: error("JNI is not Oracle standard")
     return memScoped {
         val ptr = args.toCArray(this)
-        method.invoke(this@getStaticCharMethod, clazz, methodId, ptr)
+        method.invoke(this@callStaticCharMethod, clazz, methodId, ptr)
     }.toInt().toChar()
 }
 
-fun CPointer<JNIEnvVar>.getStaticByteMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Byte {
+fun CPointer<JNIEnvVar>.callStaticByteMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Byte {
     val method = pointed.pointed?.CallStaticByteMethodA ?: error("JNI is not Oracle standard")
     return memScoped {
         val ptr = args.toCArray(this)
-        method.invoke(this@getStaticByteMethod, clazz, methodId, ptr)
+        method.invoke(this@callStaticByteMethod, clazz, methodId, ptr)
     }
 }
 
-fun CPointer<JNIEnvVar>.getStaticBooleanMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Boolean {
+fun CPointer<JNIEnvVar>.callStaticBooleanMethod(clazz: jclass, methodId: jmethodID, vararg args: jvalue): Boolean {
     val method = pointed.pointed?.CallStaticBooleanMethodA ?: error("JNI is not Oracle standard")
     return memScoped {
         val ptr = args.toCArray(this)
-        method.invoke(this@getStaticBooleanMethod, clazz, methodId, ptr)
+        method.invoke(this@callStaticBooleanMethod, clazz, methodId, ptr)
     }.toBoolean()
 }
