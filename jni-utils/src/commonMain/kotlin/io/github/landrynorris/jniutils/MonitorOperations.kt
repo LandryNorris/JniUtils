92e69ece74d74b2339fb22ce49483229bcb117dc
Landry Norris <landry.norris@live.com>
2022-07-11 21:50:26 -0500

    Added docs

--- jni-utils/src/commonMain/kotlin/io/github/landrynorris/jniutils/MonitorOperations.kt
+++ jni-utils/src/commonMain/kotlin/io/github/landrynorris/jniutils/MonitorOperations.kt
@@ -5,11 +5,21 @@ import platform.android.JNIEnvVar
 import platform.android.JNINativeMethod
 import platform.android.jobject
 
+/**
+ * Enter the monitor owned by obj
+ *
+ * Do not mix JNI [monitorExit] with Java synchronized method
+ */
 fun CPointer<JNIEnvVar>.monitorEnter(obj: jobject) {
     val method = pointed.pointed?.MonitorEnter ?: error("JNI is not Oracle standard")
     method.invoke(this, obj)
 }
 
+/**
+ * The current thread must be the owner of the monitor associated with obj.
+ *
+ * Do not mix JNI [monitorExit] with Java synchronized method
+ */
 fun CPointer<JNIEnvVar>.monitorExit(obj: jobject) {
     val method = pointed.pointed?.MonitorExit ?: error("JNI is not Oracle standard")
     method.invoke(this, obj)
